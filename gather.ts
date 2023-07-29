28348863fc9b377ae7ae1a079480b9af5c2b6a07
HIRAO53710 <naminami53710@gmail.com>
2023-07-29 11:47:12 +0900

    HTMLファイルを更新

--- gather.ts
+++ gather.ts
@@ -1,2 +1,16 @@
-//ここにGatherのプログラムを書く
-console.log("Hello World!");
+import ky from "https://cdn.skypack.dev/ky@0.28.1?dts";
+// const SPACE_ID = "適当なやつ"
+// const API_KEY = "入力する"
+// Deno.env.get("変数名")
+await ky.post(
+  "https://api.gather.town/api/v2/spaces",
+  {
+    sourceSpace: SPACE_ID,
+    name: "my new space",
+  },
+  {
+    headers: {
+      apiKey: API_KEY,
+    },
+  },
+);
